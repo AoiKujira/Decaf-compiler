d45e240c40c3b0edbddf815b4a015b4650b418ca
haniyeh <ehsanihani30@yahoo.com>
2020-06-17 23:10:36 +0430

    seperate

--- cpp2/lark_test.py
+++ cpp2/lark_test.py
@@ -10,9 +10,10 @@ def check_errors(text, output_file):
     open_file = open("grammar.txt", "r")
     grammar = open_file.read()
     parser = Lark(grammar, parser='lalr', debug=False)
-    #parser.parse(text)
+
     try:
         parser.parse(text)
         output_file.write("YES")
     except:
         output_file.write("NO")
+    #parser.parse(text)
