25d6358d008e25b1a4e74dec97a3030f63cea494
farzamzohdi <farzamzohdi@gmail.com>
2020-08-06 20:18:14 +0430

    assignments are handled exept a = alloc t

--- cpp2/mipsGen.py
+++ cpp2/mipsGen.py
@@ -12,6 +12,7 @@ assign a b= false
 //regular
 assign a = b
 assign *(a) = b
+assign a = *(b)
 assign a = allocate 6
 assign a = allocate t [panics]
 ----------arith-------------
@@ -44,7 +45,7 @@ def mipsGen(input_code):
     vars = {}
     instructions = input_code.split('\n')
     mipsDataCode = '.data\n'
-    mipsTextCode = '.text\nglobl main\n'
+    mipsTextCode = '.text\n.globl main\n'
     mipsTextCode += '################ MACROS ################\n'
     mipsTextCode += '.macro read_int($dReg)\nli	$v0, 5\nsyscall\nmove	$dReg, $v0\n.end_macro\n'
     mipsTextCode += '.macro read_string($string_address)\nli	$v0, 8\nli	$a1, 1000  #MAX_SIZE==999\nmove	$a0, $string_address\nsyscall\n.end_macro\n'
@@ -55,8 +56,8 @@ def mipsGen(input_code):
     for instruction in instructions:
         if instruction == '':
             continue
-        mipsTextCode += '#' + instruction + '\n'
-        instruction = instruction.split()
+        # mipsTextCode += '#' + instruction + '\n'
+        instruction = instruction.split(' ')
         if instruction[0] == 'arith':#arith a = b Xop c
             if instruction[4] == '+':#add
                 pass
@@ -93,29 +94,24 @@ def mipsGen(input_code):
         #what options are there?
         if instruction[0] == 'Print':#Print a
             pass
-        if instruction[0] == 'assign':#assign a f= 1.2
-            if instruction[2] == 'f=':
-                if instruction[1] in vars.keys():
-                    vars[instruction[1]] += 1
-                    instruction[1] += '___' + vars[instruction[1]]
+        if instruction[0] == 'assign':
+            if instruction[2] == 'f=':#assign a f= 1.2
                 mipsDataCode += instruction[1] + ': ' + '.double ' + instruction[3] + '\n'
                 vars[instruction[1]] = 0
             if instruction[2] == 'i=':#assign a i= 12
-                if instruction[1] in vars.keys():
-                    vars[instruction[1]] += 1
-                    instruction[1] += '___' + vars[instruction[1]]
                 mipsDataCode += instruction[1] + ': ' + '.word ' + instruction[3] + '\n'
                 vars[instruction[1]] = 0
             if instruction[2] == 's=':#assign a s= "fwrefwerf"
-                if instruction[1] in vars.keys():
-                    vars[instruction[1]] += 1
-                    instruction[1] += '___' + vars[instruction[1]]
-                mipsDataCode += instruction[1] + ': ' + '.asciiz ' + instruction[3] + '\n'
+                s = ''
+                for i in instruction[3:]:
+                    s += ' ' + i
+                mipsDataCode += '___' + instruction[1] + '___: ' + '.asciiz ' + s + '\n'
+                mipsDataCode += instruction[1] + ': ' + '.word\n'
+                mipsTextCode += 'la $t9, ' + '___' + instruction[1] + '___\n'
+                mipsTextCode += 'sw $t9, ' + instruction[1] + '\n'
+                vars['___' + instruction[1] + '___'] = 0
                 vars[instruction[1]] = 0
             if instruction[2] == 'b=':#assign a b= false
-                if instruction[1] in vars.keys():
-                    vars[instruction[1]] += 1
-                    instruction[1] += '___' + vars[instruction[1]]
                 if instruction[3] == 'false':
                     instruction[3] = '0'
                 if instruction[3] == 'true':
@@ -123,10 +119,27 @@ def mipsGen(input_code):
                 mipsDataCode += instruction[1] + ': ' + '.word ' + instruction[3] + '\n'
                 vars[instruction[1]] = 0
             if instruction[2] == '=':#assign t1 = t2
-                if instruction[1][0] == '*':
+                if instruction[3] == 'allocate':
+                    allocSize = int(instruction[4])*4
+                    mipsDataCode += instruction[1] + ': ' + '.space ' + str(allocSize) + '\n'
+                    vars[instruction[1]] = 0
                     pass
-                else:
-                    pass
+                elif instruction[1][0] == '*':#assign *(t1) = t2
+                    mipsTextCode += 'lw $t9, ' + instruction[1][2:-1] + '\n'
+                    mipsTextCode += 'lw $t8, ' + instruction[3] + '\n'
+                    mipsTextCode += 'sw $t8, ($t9)\n'
+                elif instruction[3][0] == '*':#assign t1 = *(t2)
+                    if not instruction[1] in vars.keys():
+                        mipsDataCode += instruction[1] + ': ' + '.word\n'
+                        vars[instruction[1]] = 0
+                    mipsTextCode += 'la $t9, ' + instruction[3][2:-1] + '\n'
+                    mipsTextCode += 'sw $t9, ' + instruction[1] + '\n'
+                else:#assign t1 = *t2
+                    if not instruction[1] in vars.keys():
+                        mipsDataCode += instruction[1] + ': ' + '.word\n'
+                        vars[instruction[1]] = 0
+                    mipsTextCode += "lw $t9, " + instruction[3] + '\n'
+                    mipsTextCode += "sw $t9, " + instruction[1] + '\n'
         if instruction[0] == 'getAddress':
             pass
         if instruction[0] == 'return':
