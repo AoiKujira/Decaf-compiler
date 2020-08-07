f8290e2fdce9b39f80998baf04ed4163cdbdba89
haniyeh <ehsanihani30@yahoo.com>
2020-08-07 18:49:14 +0430

    \n

--- cpp2/transformer.py
+++ cpp2/transformer.py
@@ -518,6 +518,7 @@ class Test(Transformer):
 
     def print_end(self, args):
         self.is_printing = False
+        self.code += "Prints \n"
 
     def exp_le(self, args):
         t = self.make_temp()
