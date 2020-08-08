22b0cb0b1112c534befecd1d0acd16cdfbfe1e29
farzamzohdi <farzamzohdi@gmail.com>
2020-08-08 15:45:05 +0430

    endofworld first try

--- cpp2/mipsGen.py
+++ cpp2/mipsGen.py
@@ -413,7 +413,7 @@ def mipsGen(input_code):
                 mipsTextCode += 'jr $ra\n'
             else:#lable:
                 if instruction[0] == 'main:':
-                    mipsTextCode += '___main___:\nla $ra, _______End_Of_The_World_______\n'
+                    mipsTextCode += '___main___:\nla $ra, _______EndOfTheWorld_______\n'
                 else:
                     mipsTextCode += instruction[0] + '\n'
         if instruction[0] == 'push':#push a
@@ -538,5 +538,5 @@ def mipsGen(input_code):
         if instruction[0] == 'Ifz':#Ifz a goto lable
             mipsTextCode += 'lw $t9, ' + instruction[1] + '\n'
             mipsTextCode += 'beqz $t9, ' + instruction[3] + '\n'
-    mipsTextCode += '_______End_Of_The_World_______:\nli	$v0, 10\nsyscall\n'
+    mipsTextCode += '_______EndOfTheWorld_______:\nli	$v0, 10\nsyscall\n'
     return  mipsDataCode + '\n' + mipsTextCode
