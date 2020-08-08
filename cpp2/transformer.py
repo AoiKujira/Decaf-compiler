76b285922c6a774d1c2c8a1742e4336303ca4515
haniyeh <ehsanihani30@yahoo.com>
2020-08-08 11:58:48 +0430

    :|

--- cpp2/transformer.py
+++ cpp2/transformer.py
@@ -669,6 +669,7 @@ class Test(Transformer):
             temp = args[0]
             if self.function_types_specific.__contains__(add):
                 temp = self.make_temp()
+                self.var_types[temp] = self.function_types_specific[add]
                 self.code += "pop " + temp + "\n"
                 self.code += "popra\n"
             return temp
@@ -711,6 +712,7 @@ class Test(Transformer):
             t = ""
             if self.function_types[args[0][1]] == "return":
                 t = self.make_temp()
+                self.var_types[t] = self.function_types_specific[args[0][1]]
                 self.code += "pop " + t + "\n"
                 self.code += "popra\n"
             return t
@@ -954,6 +956,10 @@ class Test(Transformer):
             last_line = line
             line = total_code[:total_code.find("\n") + len("\n")]
             total_code = total_code[total_code.find("\n") + len("\n"):]
+            if last_line.count("Print "):
+                t = last_line[len("Print "):]
+                t = t[:t.find("\n")]
+                last_line = self.print_stmt([None, [t], None])
             if (not total_code.__contains__("Print") or
                 (total_code.find("Print") > total_code.find("Lcall") and total_code.__contains__("Lcall"))) \
                     and not line.__contains__("return") and \
@@ -1150,11 +1156,13 @@ class Test(Transformer):
 
     def read_line(self, args):
         t = self.make_temp()
+        self.var_types[t] = "string"
         self.code += "ReadLine " + t + " = ReadLine()" + "\n"
         return t
 
     def read_int(self, args):
         t = self.make_temp()
+        self.var_types[t] = "int"
         self.code += "ReadInt " + t + " = ReadInt()" + "\n"
         return t
 
