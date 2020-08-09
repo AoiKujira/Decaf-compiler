2b64b4c2fbd897a1d0c73627ceebf77a6132712b
Gnomy17 <gnomy@ce.sharif.edu>
2020-08-09 10:47:44 +0430

    the sag saga begins

---
+++ cpp2/tests2/t014-the-sag-saga.d
@@ -0,0 +1,32 @@
+class Dog{
+    int[] wetness;
+    int thesaga;
+
+    int return_thing(int ind){
+        return this.wetness[ind];
+    }
+}
+
+class Wet{
+    Dog[] dogness;
+    int wetdog;
+
+    int getwetdog(){
+        return this.wetdog;
+    }
+}
+int main(){
+    Dog a;
+    Wet b;
+    int c;
+    c = ReadInteger();
+    a = new Dog;
+    b = new Wet;
+    a.wetness = NewArray(5, int);
+    a.thesaga = 56;
+    a.wetness[c] = 5;
+    b.dogness = NewArray(10, Dog);
+    b.wetdog = 2;
+    b.dogness[5] = a;
+    Print(b.dogness[a.return_thing(b.getwetdog())].thesaga);
+}
